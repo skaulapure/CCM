988f2c360ce3b28d7d7dd97584b728b21b9deaba
skaulapure <skaulapure@hawk.iit.edu>
2017-03-14 14:12:36 -0500

    Update garch.py

--- garch.py
+++ garch.py
@@ -1,95 +1,130 @@
-'''Initial code for the garch model. I still need to use functions and incorporate a way to solve for gamma, alpha and beta. '''
-
-from pymongo import MongoClient
 import pandas as pd
 import numpy as np
-import math
-import scipy.optimize as sci
+from scipy import optimize
+import random
+from scipy.stats import norm
+#import matplotlib as plt
+#import seaborn as sns
+from pymongo import MongoClient
+
+class GARCH(object):
+    def __init__(self, gamma, alpha):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.beta = 1 - alpha
+
+    def step(self, log_return, previous_var):
+        return self.gamma + self.alpha*log_return**2 + self.beta*previous_var
+
+#class MCS(object):
 
 
-# def sol(alpha):
-#   beta = 1 - alpha
+def data_call(a,b):
+    connection = MongoClient()
+    db = connection.ccm
+    data = list(db.monte_carlo.find({}, {'_id': 0, 'Future': 1, 'AtM': 1, 'Date': 1}).sort('Date'))
+    df_columns = ['Date', 'Future', 'AtM', 'log_returns', 'AtM_Var', 'Norm_AtM_Vol',
+                  'stochastic_factor', 'simulated_var', 'simulated_AtMVol', 'simulated_futures',
+                  'simulated_log_returns', 'error', 'abs_error', 'vol_error', 'abs_vol_error']
 
-#  return beta
+    df = pd.DataFrame(data, columns=df_columns)
+    df.set_index('Date', inplace=True)
+    #df = df.loc['2009-06-15':'2014-12-31']
+    df = df.loc[a:b]
 
-def calc(c, df):
-    alpha = c[0]
-    gamma = c[1]
-    df.log_returns = df.Future.pct_change()
+    df.log_returns = np.log(df.Future / df.Future.shift())
+    df.AtM_Var = df.AtM * df.AtM
+    df.Norm_AtM_Vol = df.AtM / 16
+#    log_mean = df.log_returns.mean()
+    # norm_AtMVol = df.Norm_AtM_Vol.mean()
+#    df.stochastic_factor = (df.log_returns - log_mean) / df.Norm_AtM_Vol
 
-    df.AtM_Var = df['AtM'] ** 2
+    return df
 
-    df.Norm_AtM_Vol = df.AtM.divide(math.sqrt(252))
 
+def mean_stochastic_factor(df, df1):
     log_mean = df.log_returns.mean()
-    norm_AtMVol = df.Norm_AtM_Vol.mean()
+    # norm_AtMVol = df.Norm_AtM_Vol.mean()
+    df.stochastic_factor = (df.log_returns - log_mean) / df.Norm_AtM_Vol
+    df1.stochastic_factor = (df1.log_returns - log_mean) / df1.Norm_AtM_Vol
 
-    df.stocastic_factor = (df.log_returns - log_mean).divide(df.Norm_AtM_Vol)
+    return df, df1
 
-    df.simulated_var[1] = df.AtM_Var[1]
 
-    df.simulated_futures[1] = df.Future[1]
+def calc(guess, df):
+    gamma = guess[0]
+    alpha = guess[1]
+    log_mean = df.log_returns.mean()
 
-    df.simulated_log_returns[1] = df.log_returns[1]
+    var = df.AtM_Var[1]
+    atm_vol = df.AtM[1]
+    future = df.Future[1]
+    log_return = df.log_returns[1]
 
-    df.simulated_AtMVol[1] = df.simulated_var[1] ** .5
+    simulated_var = [var, ]
+    simulated_future = [future, ]
+    simulated_log_return = [log_return, ]
+    simulated_atm_vol = [atm_vol, ]
 
-    # values of gamma, alpha, beta taken from prototype
-    # gamma = 0.0003343
-    # alpha = 0.0072841
-    # beta = 0.9927159
-    beta = 1 - alpha
+    garch = GARCH(gamma, alpha)
 
-    for i in range(2, len(df.index)):
-        df.simulated_var[i] = i
+    for stochastic in df.stochastic_factor[2:]:
+        var = garch.step(log_return, var)
+        atm_vol = np.sqrt(var)
+        log_return = log_mean + stochastic * atm_vol/16
+        future = future*np.exp(log_return)
+        simulated_future.append(future)
+        simulated_var.append(var)
+        simulated_atm_vol.append(atm_vol)
+        simulated_log_return.append(log_return)
 
-        df.simulated_var[i] = gamma + alpha * (df.simulated_log_returns[i - 1] - log_mean) ** 2 + (
-        beta * df.simulated_var[i - 1])
+    error = np.array([i - j for i, j in zip(df.log_returns[1:], simulated_log_return)])
+    vol_error = np.array([i - j for i, j in zip(df.AtM[1:], simulated_atm_vol)])
+    abs_error = abs(error)
+    abs_vol_error = abs(vol_error)
 
-        df.simulated_AtMVol[i] = df.simulated_var[i] ** .5
+    df.simulated_futures[1:] = simulated_future
+    df.simulated_AtMVol[1:] = simulated_atm_vol
+    df.simulated_log_returns[1:] = simulated_log_return
+    df.simulated_var[1:] = simulated_var
 
-        df.simulated_futures[i] = df.simulated_futures[i - 1] * np.exp(log_mean \
-                                                                       + (df.stocastic_factor[i] * df.simulated_AtMVol[
-            i] / 16))
+    df.error[1:] = error
+    df.abs_error[1:] = abs_error
+    df.vol_error[1:] = vol_error
+    df.abs_vol_error[1:] = abs_vol_error
+    df.plot(y = 'error')
+    #df.plot(x=df.Date, y='error')#, y='vol_error')
+    return df, abs_error.mean()
 
-        df.simulated_log_returns[i] = math.log(df.simulated_futures[i] / df.simulated_futures[i - 1])
 
-    df.error = df.log_returns - df.simulated_log_returns
-
-    df.vol_error = df.AtM - df.simulated_AtMVol
-    df.abs_error = df.error.abs()
-
-    df.abs_vol_error = df.vol_error.abs()
-    #print(df.abs_error.mean())
-    print(alpha, beta, gamma)
-    return (df.abs_error.mean())
-
+def real_stochastic(df1):
+    
+    factor = [random.random() for i in range(1, len(df1.index))]
+    factor_inv = np.array([norm.ppf(factor[i]) for i in range(0, len(factor))])
+    df1.stimulated_stochastic_factor[1:] = factor_inv
+    
+    return df1
 
 def main():
-    connection = MongoClient()
-
-    db = connection.ccm
-
-    data = list(db.monte_carlo.find({}, {'_id': 0, 'Future': 1, 'AtM': 1, 'Date': 1}).sort('Date'))
-
-    df = pd.DataFrame(data, columns=['Date', 'Future', 'AtM', 'log_returns', 'AtM_Var', 'Norm_AtM_Vol',
-                                     'stocastic_factor', 'simulated_var', 'simulated_AtMVol', 'simulated_futures',
-                                     'simulated_log_returns', 'error', 'abs_error', 'vol_error', 'abs_vol_error'])
-
-    df.set_index('Date', inplace=True)
-
-    df = df.loc['2009-06-15':'2014-12-31']
-
+    df_list = ['2009-06-15', '2014-12-31', '2016-12-13']
+    df = data_call(df_list[0], df_list[1])
     # gamma = 0.0003343
     # alpha = 0.0072841
+    df1 = data_call(df_list[1], df_list[0])
 
-    # beta = sol(alpha)
-    sci.minimize(calc, [0,0], df)
-    # beta = sol(alpha)
-    #    calc (alpha, gamma, beta, df)
+    df, df1 = mean_stochastic_factor(df, df1)
 
-    return
+    df1 = real_stochastic(df1)
+
+    initial_guess = np.array([0, 0])
+
+    cons = [{'type': 'ineq', 'fun': lambda x: x - 0},
+            {'type': 'ineq', 'fun': lambda x: x - 0}]
+    df, result = optimize.minimize(calc, initial_guess, (df,), constraints=cons)
+    df.plot(y='error')
+    return result
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    sols = main()
+    print(sols)
